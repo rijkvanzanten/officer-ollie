209456a161611b9379d47538e1c2b79e3b9a4f54
rijkvanzanten <rijkvanzanten@me.com>
2018-12-15 00:34:08 -0500

    Force auto deploys

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd dist;
 git init;
 git add .;
 git commit -m "Deploy";
-git push git@github.com:rijkvanzanten/officer-ollie.git master:gh-pages;
+git push git@github.com:rijkvanzanten/officer-ollie.git master:gh-pages --force;
