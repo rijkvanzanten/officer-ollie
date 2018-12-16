e2dd5ed4302f3d7e342f6db1b0c601fadbe270d8
rijkvanzanten <rijkvanzanten@me.com>
2018-12-16 15:09:51 -0500

    Add cname to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 npm run build;
 cd dist;
 git init;
+echo "officerollie.rijks.work" >> CNAME;
 git add .;
 git commit -m "Deploy";
 git push git@github.com:rijkvanzanten/officer-ollie.git master:gh-pages --force;
